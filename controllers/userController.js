467c09c9a0835262ebf5a9a19ef8274b1c4798f5
J <59581793+daechi73@users.noreply.github.com>
2024-01-05 00:06:53 -0500

    implemented user_sign_up

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user.js");
 const passport = require("passport");
+const HashedPassword = require("../public/javascripts/HashPassword.js");
+const { body, validationResult } = require("express-validator");
 
 exports.users_get = asyncHandler(async (req, res, next) => {
   const users = await User.find();
@@ -43,3 +45,51 @@ exports.user_sign_out = asyncHandler(async (req, res, next) => {
     res.json({ status: "success", user: req.user });
   });
 });
+
+exports.sign_up_post = [
+  body("name")
+    .trim()
+    .exists()
+    .withMessage("You must Enter a first name")
+    .isLength({ min: 3 })
+    .withMessage("name has to be atleast 3 characters long"),
+  body("username")
+    .trim()
+    .exists()
+    .withMessage("You must Enter a username")
+    .isLength({ min: 3 })
+    .withMessage("Username has to be atleast 3 characters long")
+    .custom(async (value) => {
+      const user = await User.findOne({ user_name: value }).exec();
+      if (user) {
+        throw new Error("Username already in use");
+      }
+    }),
+  body("password")
+    .trim()
+    .exists()
+    .withMessage("You must enter a password")
+    .isStrongPassword({
+      minLength: 8,
+    })
+    .withMessage("Password must be atleast 8 charaters."),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const user = new User({
+      name: req.body.name,
+      user_name: req.body.username,
+      password: await HashedPassword(req.body.password),
+    });
+    if (!errors.isEmpty()) {
+      res.json({
+        status: "failed",
+        user: user,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await user.save();
+      res.json({ status: "success", user: user });
+    }
+  }),
+];
