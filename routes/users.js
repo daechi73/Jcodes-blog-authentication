c13f0e5481333a4226673519264fd8fea09c9c63
J <59581793+daechi73@users.noreply.github.com>
2024-01-04 23:43:02 -0500

    routed /users/sign-up

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,9 @@ var userController = require("../controllers/userController");
 
 /* GET users listing. */
 router.get("/", userController.users_get);
-
 router.post("/sign-up", userController.user_sign_in);
-
 router.get("/sign-out", userController.user_sign_out);
-
+router.post("/sign-up", userController.user_sign_up);
 router.get("/:id", userController.user_detail);
 
 module.exports = router;
