6843d6d8fc43794370ed62d6e2ff41b0ecc7484e
J <59581793+daechi73@users.noreply.github.com>
2024-01-08 20:22:22 -0500

    fixed method called

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var userController = require("../controllers/userController");
 
 /* GET users listing. */
 router.get("/", userController.users_get);
-router.post("/sign-up", userController.user_sign_in);
+router.post("/sign-in", userController.user_sign_in);
 router.get("/sign-out", userController.user_sign_out);
 router.post("/sign-up", userController.user_sign_up);
 router.get("/:id", userController.user_detail);
