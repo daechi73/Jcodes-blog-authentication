98547f96d6a4ec6a98fa81760de2d8df09d30ff2
J <59581793+daechi73@users.noreply.github.com>
2024-01-10 14:32:52 -0500

    fixed to send json

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,7 +3,11 @@ const Comment = require("../models/comment");
 
 exports.comment_list = asyncHandler(async (req, res, next) => {
   const comment = await Comment.find().exec();
-  if (comment.length === 0) res.send("No Comments Found");
-  res.send(comment);
+  if (comment.length === 0) res.json("No Comments Found");
+  res.json(comment);
 });
-exports.comment_post = [asyncHandler(async (req, res, next) => {})];
+exports.comment_post = [
+  asyncHandler(async (req, res, next) => {
+    res.json("here");
+  }),
+];
