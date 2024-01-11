337b75865652e793a8e6ddc72e4f31b947d40179
J <59581793+daechi73@users.noreply.github.com>
2024-01-10 20:06:46 -0500

    implemented comment post api

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,13 +1,36 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 const Comment = require("../models/comment");
+const user = require("../models/user");
 
 exports.comment_list = asyncHandler(async (req, res, next) => {
   const comment = await Comment.find().exec();
-  if (comment.length === 0) res.json("No Comments Found");
-  res.json(comment);
+  if (comment.length === 0) return res.json("No Comments Found");
+  return res.json(comment);
 });
 exports.comment_post = [
+  // body("comment")
+  //   .trim()
+  //   .isLength({ min: 4 })
+  //   .withMessage("You must type atleast 4 letters"),
   asyncHandler(async (req, res, next) => {
-    res.json("here");
+    const errors = validationResult(req);
+    const comment = new Comment({
+      user: req.body.user,
+      comment: req.body.comment,
+    });
+    if (!errors.isEmpty()) {
+      res.json({
+        status: "failed",
+        user: user,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await comment.save();
+      res.json({
+        status: "success",
+      });
+    }
   }),
 ];
