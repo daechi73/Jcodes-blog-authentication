64d241e9e83cf66214d9c1fa4e10e643b4ee6bd3
J <59581793+daechi73@users.noreply.github.com>
2024-01-11 23:57:49 -0500

    changed virtual property: date_added_formatted to return the hours as well

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  date_added: { type: Date, Default: Date.now },
+  date_added: { type: Date, default: Date.now },
   comment: { type: String, required: true, maxLength: 400 },
 });
 
@@ -13,7 +13,9 @@ CommentSchema.virtual("url").get(function () {
 });
 
 CommentSchema.virtual("date_added_formatted").get(function () {
-  return DateTime.fromJSDate(this.date_added).toLocaleString(DateTime.DATE_MED);
+  return DateTime.fromJSDate(this.date_added).toLocaleString(
+    DateTime.DATETIME_MED
+  );
 });
-
+CommentSchema.set("toJSON", { virtuals: true });
 module.exports = mongoose.model("Comment", CommentSchema);
