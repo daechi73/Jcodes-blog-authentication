6748f483bde863dfaf8979feae86f574af998dc0
J <59581793+daechi73@users.noreply.github.com.>
2024-11-13 01:14:13 -0500

    added login persist dev api

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,11 @@ router.get("/", userController.users_get);
 router.post("/sign-in", userController.user_sign_in);
 router.get("/sign-out", userController.user_sign_out);
 router.post("/sign-up", userController.user_sign_up);
+router.get("/check", (req, res) => {
+  req.isAuthenticated()
+    ? res.json("Login persist works")
+    : res.json("Persist is not working");
+});
 router.get("/:id", userController.user_detail);
 
 module.exports = router;
